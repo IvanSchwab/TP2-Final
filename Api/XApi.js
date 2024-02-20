b6c58f9208835e07a1870524dafba777520c8be5
Iván <106625134+IvanSchwab@users.noreply.github.com>
2024-02-20 00:58:29 -0300

    .

--- Api/XApi.js
+++ Api/XApi.js
@@ -1,12 +1,31 @@
+import Factory from "../DAOs/Factory.js";
+import { MODO } from "../Config/config.js";
+
 class XApi {
   constructor() {
-    this.memory = [];
+    this.factory = Factory.factory(MODO);
   }
 
   create = async (info) => {
     try {
-      this.memory.push(info);
-      return await info;
+      const data = await this.factory.xDao.create(info);
+      return await data;
+    } catch (error) {
+      throw error;
+    }
+  };
+  getAll = async () => {
+    try {
+      const data = await this.factory.xDao.getAll();
+      return await data;
+    } catch (error) {
+      throw error;
+    }
+  };
+  getAmount = async (amount) => {
+    try {
+      const data = await this.factory.xDao.getAmount(amount);
+      return await data;
     } catch (error) {
       throw error;
     }
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { SERVER_PORT } from "./config/config.js";
+import { SERVER_PORT } from "./Config/config.js";
 import router from "./routes/router.js";
 
 const app = express();
 
-app.use("/api", router);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api", router);
 
 app.listen(SERVER_PORT, () => {
-  console.log("Server conectado");
+  console.log("Server OK");
 });
